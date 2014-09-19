217aabe30d1d739b73068add162db7e0c69ef54a
tnqt22 <tnguye13@ggc.edu>
2014-09-19 16:19:48 -0400

    Create Pizza.java
    Created Pizza class for storing pizza info

---
+++ Pizza.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Pizza
+{
+  private boolean hasCheese;
+  private ArrayList<Topping> toppings;
+  private double price;
+  
+  public Pizza(double price)
+  {
+    this.price = price;
+  }
+  
+  // TODO: please implement rest tomorrow
+}
